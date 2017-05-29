547326d1a35e725bc7d20a61fbae2a96944a6aea
omerk <omer@stuccomedia.com>
2017-05-29 20:19:52 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+﻿# AdWords SDK Asyncer
+
+This small console app, allows to automate the process of creating async method in the core class of AdWords API (`AdWordsApi.cs`)
+
+
+## Setup
+
+>First step, you will need to manually add the async invoker to the `Common/Lib/AdsSoapClient.cs` :  
+The `Common/Lib/AdsSoapClient.cs` class is shared between all versions so this is a one time addition.  
+```csharp
+protected async Task<object[]> InvokeAsync(string methodName, object[] parameters)
+{
+    var action = new Func<AsyncCallback, object, IAsyncResult>((y, x) => { return base.BeginInvoke(methodName, parameters, y, x); });
+
+    while (true)
+    {
+        InitForCall(methodName, parameters);
+
+        try
+        {
+            return
+                await Task.Factory.FromAsync(action,
+                    base.EndInvoke,
+                    null);
+        }
+        catch (SoapException ex)
+        {
+            ErrorHandler errorHandler = CreateErrorHandler();
+
+            Exception customException = GetCustomException(ex);
+            if (errorHandler.ShouldRetry(customException))
+            {
+                errorHandler.PrepareForRetry(customException);
+            }
+            else
+                throw customException;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+        finally
+        {
+            CleanupAfterCall(methodName, parameters);
+        }
+    }
+}
+```
+
+
+>Second step is to run the upgrader, to create all async methods (`get`,`mutate`,`query`,`getResult`)  
+Simply by the following command:
+`> Upgrader.exe ./AdWords/<ApiVersion>/AdWordsApi.cs`
+
+>Third step, is to compile the library and enjoy (`getAsync`,`mutateAsync`,`queryAsync`,`getResultAsync`)  
+which will now be added to all Adwords Services.
+
+
---
+++ Upgrader/Program.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdWordsUpgrader
+{
+    class Asyncer : CSharpSyntaxRewriter
+    {
+
+        public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            foreach(var mt in node.Members.Select(x => x as MethodDeclarationSyntax).Where(x => x!= null))
+            {
+                var id = mt.Identifier.ToString();
+
+                if (id != "get" && id != "mutate" && id != "query" && id != "getResult")
+                    continue;
+
+                var asyncMethod = ConvertToAsync(mt);
+
+                node = node.AddMembers(asyncMethod);
+
+            }
+
+            return node;
+        }
+
+        private MethodDeclarationSyntax ConvertToAsync(MethodDeclarationSyntax node)
+        {
+            var nm = CSharpSyntaxTree.ParseText(@"public void m3()
+            { 
+                Task<object[]> results = this.InvokeAsync(""" + node.Identifier.ToString() +
+                @""", new object[]
+              {" +
+
+                string.Join(",", node.ParameterList.Parameters.Select(x => x.Identifier.ToString()))
+               +
+              @"}); 
+              return ((" + node.ReturnType.GetText().ToString().Trim() + @")((await results)[0]));
+            }
+            ").GetRoot().DescendantNodes().First() as MethodDeclarationSyntax;
+
+
+            var asyncMathod = SyntaxFactory.MethodDeclaration(node.AttributeLists, node.Modifiers,
+                SyntaxFactory.ParseTypeName(" Task<" + node.ReturnType.GetText().ToString().Trim() + "> "),
+                node.ExplicitInterfaceSpecifier,
+                SyntaxFactory.Identifier(node.Identifier.Text + "Async"), node.TypeParameterList, node.ParameterList, node.ConstraintClauses,
+                nm.Body
+                    , node.ExpressionBody
+                )
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.AsyncKeyword))
+              ;
+
+            return asyncMathod;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csFile = args[0];
+                
+            var tree = CSharpSyntaxTree.ParseText(File.ReadAllText(csFile));
+
+            var rewriter = new Asyncer();
+            var result = rewriter.Visit(tree.GetRoot());
+
+            File.Move(csFile, csFile + ".origin");
+
+            File.WriteAllText(csFile, result.ToFullString());
+
+        }
+
+    }
+}
